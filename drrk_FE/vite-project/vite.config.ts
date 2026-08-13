e5e156ec9858810f7a8b0504155024698b31751b
hingbing <tjwodnjs04@gmail.com>
2026-08-13 09:31:55 +0900

    상하 레이아웃 제작

--- drrk_FE/vite-project/vite.config.ts
+++ drrk_FE/vite-project/vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig, mergeConfig } from 'vite'
-import { defineConfig as defineVitestConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineVitestConfig({
-    test: {
-      environment: 'jsdom',
-      globals: true,
-    },
-  })
-)
+export default defineConfig({
+  plugins: [react()],
+});
